18701d793249268ebf3b43ff32795597b75ba034
160405103 <1348313766@qq.com>
2023-12-12 18:38:41 +0800

    9.2  Fixing the loading skeleton bug with route groups

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +15,24 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
@@ -56,3 +63,20 @@ export function Card({
     </div>
   );
 }
+
+// Deciding where to place your Suspense boundaries
+// Where you place your Suspense boundaries will depend on a few things:
+
+// How you want the user to experience the page as it streams.
+// What content you want to prioritize.
+// If the components rely on data fetching.
+// Take a look at your dashboard page, is there anything you would've done differently?
+
+// Don't worry. There isn't a right answer.
+
+// You could stream the whole page like we did with loading.tsx... but that may lead to a longer loading time if one of the components has a slow data fetch.
+// You could stream every component individually... but that may lead to UI popping into the screen as it becomes ready.
+// You could also create a staggered effect by streaming page sections. But you'll need to create wrapper components.
+// Where you place your suspense boundaries will vary depending on your application. In general, it's good practice to move your data fetches down to the components that need it, and then wrap those components in Suspense. But there is nothing wrong with streaming the sections or the whole page if that's what your application needs.
+
+// Don't be afraid to experiment with Suspense and see what works best, it's a powerful API that can help you create more delightful user experiences.
